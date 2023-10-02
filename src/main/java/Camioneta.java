ca9778028741290c93222f7a63f5a061a6af8deb
Manue-droi <manusarf16@gmail.com>
2023-10-02 18:02:10 -0500

    hlhl

--- src/main/java/Camioneta.java
+++ src/main/java/Camioneta.java
@@ -7,7 +7,7 @@ public class Camioneta extends Vehiculo {
 	
 	
 	public Camioneta(String placa, int puertas, String nombre, int precio, int peso, Fabricante fabricante, boolean volco) {
-		super( placa, puertas,90, nombre, precio, precio, peso, "4X4", fabricante);
+		super(placa, puertas, 90, nombre, precio, peso, "4X4", fabricante);
 		this.volco = volco;
 		cantidadCamionetas++;
 	}
