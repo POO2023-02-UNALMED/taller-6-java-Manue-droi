c73546fc1434c4ada4e71833f591bcf024ccabf9
Manue-droi <manusarf16@gmail.com>
2023-10-02 17:57:49 -0500

    dfdfdfdsf

--- src/main/java/Vehiculo.java
+++ src/main/java/Vehiculo.java
@@ -11,7 +11,7 @@ public class Vehiculo {
 	private Fabricante fabricante;
 	private static int CantidadVehiculos;
 	
-	public Vehiculo(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso, String traccion, String fabricante) {
+	public Vehiculo(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso, String traccion, Fabricante fabricante) {
 		this.placa = placa;
 		this.puertas = puertas;
 		this.velocidadMaxima = velocidadMaxima;
