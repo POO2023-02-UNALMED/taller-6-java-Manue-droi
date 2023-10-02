6925a89037de889af5f349f594f8ac73e2101247
Manue-droi <manusarf16@gmail.com>
2023-10-02 18:53:30 -0500

    dfd

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -40,10 +40,10 @@ public class Fabricante {
 	}
 	
 	public static  Fabricante fabricaMayorVentas () {
-		for(int i=0;i<(elegidoFab.size())-1; i++ ) {
+		for(int i=0;i<(elegidoFab.size()); i++ ) {
 			Fabricante MasVendido1=elegidoFab.get(0);
 			Fabricante MasVendido2=elegidoFab.get(i+1);
-			if (MasVendido1.getVecesFabricante()>MasVendido2.getVecesFabricante()) {
+			if (MasVendido1.getVecesFabricante() > MasVendido2.getVecesFabricante()) {
 				fabricanteElegido=MasVendido1;
 			}
 			else {
