1f6e53890bbe8c154dd13dea1bf78ff1430eb2d8
Manue-droi <manusarf16@gmail.com>
2023-10-02 17:42:33 -0500

    sdsdsd

--- src/main/java/Camion.java
+++ src/main/java/Camion.java
@@ -5,7 +5,7 @@ public class Camion extends Vehiculo {
 	protected static int cantidadCamiones;
 	
 	
-	publix Camion(String placa, String nombre, int precio, int peso, Fabricante fabricante, int ejes) {
+	public Camion(String placa, String nombre, int precio, int peso, Fabricante fabricante, int ejes) {
 		super(placa, 2, 80, nombre, precio, peso, "4X2", fabricante);
 		this.ejes=ejes;
 		cantidadCamiones++;
